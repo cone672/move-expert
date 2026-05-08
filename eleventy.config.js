6e95ff2a99113240b08807aeeb55c92a78fa0371
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2026-05-08 23:54:24 +0200

    Add HtmlBasePlugin to Eleventy configuration

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 
   return {
     htmlTemplateEngine: "njk",
